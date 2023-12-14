e2db0f847f8c3992cdd00c36e57c6625e9b3bf60
andotnct <mityugame@gmail.com>
2023-12-14 14:33:53 +0900

    複数線の描画を実装

--- src/ElmCanvas.elm
+++ src/ElmCanvas.elm
@@ -1,31 +1,38 @@
 module ElmCanvas exposing (main)
 
-import Browser exposing (sandbox)
-import Browser.Events exposing (onMouseDown)
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (on, onClick, onMouseOut, onMouseOver)
-import Json.Decode exposing (Decoder, field, float, int, map2)
+import Browser
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Json.Decode exposing (..)
 import Svg exposing (Svg, circle, rect, svg)
 import Svg.Attributes exposing (cx, cy, fill, height, r, stroke, strokeWidth, viewBox, width, x, y)
 
 
 main : Program () Model Msg
 main =
-    Browser.sandbox { init = { coords = [], color = "black", penSize = 3 }, update = update, view = view }
+    Browser.sandbox { init = { points = [], color = "black", penSize = 3, penNum = 5, isClick = False }, update = update, view = view }
 
 
 type alias CoordModel =
     { x : Float
     , y : Float
+    }
+
+
+type alias PointModel =
+    { coord : CoordModel
     , color : String
     , size : Int
     }
 
 
 type alias Model =
-    { coords : List CoordModel
+    { points : List PointModel
     , color : String
     , penSize : Int
+    , penNum : Int
+    , isClick : Bool
     }
 
 
@@ -35,16 +42,24 @@ type Msg
     | ChangeColor String
     | PlusPenSize
     | MinusPenSize
+    | PlusPenNum
+    | MinusPenNum
+    | EnablePen
+    | DisablePen
 
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
         AddPoint x y ->
-            { model | coords = { x = x + 10, y = y - 78, color = model.color, size = model.penSize } :: model.coords }
+            if model.isClick then
+                { model | points = model.points ++ addCoords model.penNum model.penNum (x + 10) (y - 78) model.color model.penSize }
+
+            else
+                model
 
         ResetPoint ->
-            { model | coords = [] }
+            { model | points = [] }
 
         ChangeColor color ->
             { model | color = color }
@@ -53,23 +68,78 @@ update msg model =
             { model | penSize = model.penSize + 1 }
 
         MinusPenSize ->
-            { model | penSize = model.penSize - 1 }
+            if model.penSize > 1 then
+                { model | penSize = model.penSize - 1 }
+
+            else
+                model
+
+        PlusPenNum ->
+            { model | penNum = model.penNum + 1 }
+
+        MinusPenNum ->
+            if model.penNum > 1 then
+                { model | penNum = model.penNum - 1 }
+
+            else
+                model
+
+        EnablePen ->
+            { model | isClick = True }
+
+        DisablePen ->
+            { model | isClick = False }
 
 
-onMouseDown function =
-    on "mousedown" (map2 function (field "clientX" float) (field "clientY" float))
+onMouseMove function =
+    on "mousemove" (map2 function (field "clientX" float) (field "clientY" float))
 
 
-viewPoint coord =
-    circle
-        [ cx (String.fromFloat coord.x)
-        , cy (String.fromFloat coord.y)
-        , r (String.fromInt coord.size)
-        , fill coord.color
-        , stroke coord.color
-        , strokeWidth "0"
-        ]
-        []
+addCoords : Int -> Int -> Float -> Float -> String -> Int -> List PointModel
+addCoords penNum remainingLineCnt x y color size =
+    if remainingLineCnt == 0 then
+        [ { coord = { x = x, y = y }, color = color, size = size } ]
+
+    else
+        { coord = rotateCoord x y penNum, color = color, size = size } :: addCoords penNum (remainingLineCnt - 1) (rotateCoord x y penNum).x (rotateCoord x y penNum).y color size
+
+
+rotateCoord x y penNum =
+    let
+        rad =
+            2 * pi / toFloat penNum
+
+        rotate_x =
+            x * cos rad - y * sin rad + 300 - 300 * cos rad + 300 * sin rad
+
+        rotate_y =
+            x * sin rad + y * cos rad + 300 - 300 * sin rad - 300 * cos rad
+    in
+    { x = rotate_x, y = rotate_y }
+
+
+viewPoint point =
+    if point.coord.x > 10 && point.coord.x < 610 && point.coord.y > 10 && point.coord.y < 610 then
+        circle
+            [ cx (String.fromFloat point.coord.x)
+            , cy (String.fromFloat point.coord.y)
+            , r (String.fromInt point.size)
+            , fill point.color
+            , stroke point.color
+            , strokeWidth "0"
+            ]
+            []
+
+    else
+        circle
+            [ cx (String.fromFloat point.coord.x)
+            , cy (String.fromFloat point.coord.y)
+            , r "0"
+            , fill point.color
+            , stroke point.color
+            , strokeWidth "0"
+            ]
+            []
 
 
 view : Model -> Html Msg
@@ -81,30 +151,48 @@ view model =
             [ Html.h1 [] [ text "「千手観音キャンバス」" ]
             , svg
                 [ viewBox "0 0 620 620"
-                , width "605"
-                , height "605"
+                , style "width" "605"
+                , style "height" "605"
                 ]
                 (List.concat
                     [ [ rect
                             [ x "10"
                             , y "10"
-                            , width "600"
-                            , height "600"
+                            , style "width" "600"
+                            , style "height" "600"
                             , fill "white"
                             , stroke "black"
                             , strokeWidth "2"
-                            , onMouseDown AddPoint
+                            , onMouseMove AddPoint
+                            , onMouseDown EnablePen
+                            , onMouseUp DisablePen
                             ]
                             []
                       ]
-                    , List.map (\coord -> viewPoint coord) model.coords
+                    , List.map (\coord -> viewPoint coord) model.points
                     ]
                 )
             , div
                 []
-                [ button
-                    [ onClick ResetPoint ]
-                    [ text "RESET" ]
+                [ text "penNum "
+                , button
+                    [ onClick MinusPenNum ]
+                    [ text "-" ]
+                , text (" " ++ String.fromInt model.penNum ++ " ")
+                , button
+                    [ onClick PlusPenNum ]
+                    [ text "+" ]
+                ]
+            , div
+                []
+                [ text "penSize "
+                , button
+                    [ onClick MinusPenSize ]
+                    [ text "-" ]
+                , text (" " ++ String.fromInt model.penSize ++ " ")
+                , button
+                    [ onClick PlusPenSize ]
+                    [ text "+" ]
                 ]
             , div
                 []
@@ -124,11 +212,8 @@ view model =
             , div
                 []
                 [ button
-                    [ onClick MinusPenSize ]
-                    [ text "-" ]
-                , button
-                    [ onClick PlusPenSize ]
-                    [ text "+" ]
+                    [ onClick ResetPoint ]
+                    [ text "RESET" ]
                 ]
             ]
         ]
